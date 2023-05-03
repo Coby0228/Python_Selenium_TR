0477f74cb22d2142882c19a56e817ccd246fc0c3
BlueEnchanter0228 <coby0228@gmail.com>
2023-05-03 23:11:38 +0800

    going to do API next time:

--- Weeb.py
+++ Weeb.py
@@ -1,14 +1,21 @@
 from script import *
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
 
-def switch_frame(element):
-    driver.switch_to.default_content()
-    driver.switch_to.frame(element)
-    
 
-driver = webdriver.Chrome()
+# def get_question_id_by_target_name(target_name):
+#     logger.debug(f'try to get question id by {target_name}')
+#     question_id = CAPTCHA_TARGET_NAME_QUESTION_ID_MAPPING.get(target_name)
+#     logger.debug(f'question_id {question_id}')
+#     return question_id
+
+#chrome setting    
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('excludeSwitches',['enable-automation'])
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip121/query")
 
 MyID_path = driver.find_element(By.ID,"pid")
@@ -20,20 +27,28 @@ End_Station_path.send_keys(End_Station)
 Button_bytime = driver.find_element(By.XPATH,"//*[@id='"'queryForm'"']/div[1]/div[1]/div[6]/div[2]/label[2]")
 Button_bytime.click()
 
-Start_time_path = driver.find_element(By.ID,"startTime1")
-Start_time_path.send_keys(Start_time)
-End_time_path = driver.find_element(By.ID,"endTime1")
-End_time_path.send_keys(End_time)
+Start_time_path = Select(driver.find_element(By.ID,"startTime1"))
+Start_time_path.select_by_value(Start_time)
+End_time_path = Select(driver.find_element(By.ID,"endTime1"))
+End_time_path.select_by_value(End_time)
 
 Traintype = driver.find_element(By.CLASS_NAME,"trainType")
 Traintype_list = Traintype.find_elements(By.CLASS_NAME,"btn.btn-lg.btn-linear")
 for i in range(len(Traintype_list)):
     Traintype_list[i].click()
-#小小機器人
+#小小機器人 1
 Captcha_frame = driver.find_element(By.XPATH,"//*[@id='"'recaptcha'"']/div/div/div/iframe")
-switch_frame(Captcha_frame)
+driver.switch_to.default_content()
+driver.switch_to.frame(Captcha_frame)
 Captcha_check = driver.find_element(By.XPATH,"//*[@id='"'recaptcha-anchor'"']/div[1]")
 Captcha_check.click()
+time.sleep(2)
+#小小機器人 2
+driver.switch_to.default_content()
+Captcha_inner_frame = driver.find_element(By.XPATH,"/html/body/div[8]/div[4]/iframe")
+driver.switch_to.frame(Captcha_inner_frame)
+#中文
+AI_type = driver.find_element(By.XPATH,"//*[@id='"'rc-imageselect'"']/div[2]/div[1]/div[1]/div/strong").text
 
-time.sleep(3)
+time.sleep(6)
 driver.close() 
--- script.py
+++ script.py
@@ -4,3 +4,26 @@ Start_Station = "1080-桃園"
 End_Station = "2220-清水"
 Start_time = "09:00"
 End_time = "12:00"
+
+CAPTCHA_TARGET_NAME_QUESTION_ID_MAPPING = {
+    "taxis": "/m/0pg52",
+    "bus": "/m/01bjv",
+    "school bus": "/m/02yvhj",
+    "motorcycles": "/m/04_sv",
+    "tractors": "/m/013xlm",
+    "chimneys": "/m/01jk_4",
+    "crosswalks": "/m/014xcs",
+    "traffic lights": "/m/015qff",
+    "bicycles": "/m/0199g",
+    "parking meters": "/m/015qbp",
+    "cars": "/m/0k4j",
+    "vehicles": "/m/0k4j",
+    "bridges": "/m/015kr",
+    "boats": "/m/019jd",
+    "palm trees": "/m/0cdl1",
+    "mountains or hills": "/m/09d_r",
+    "fire hydrant": "/m/01pns0",
+    "fire hydrants": "/m/01pns0",
+    "a fire hydrant": "/m/01pns0",
+    "stairs": "/m/01lynh",
+}
