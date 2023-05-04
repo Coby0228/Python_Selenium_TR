5800b258bccf7acb64fa2e1043d0c8c3408d3453
BlueEnchanter0228 <coby0228@gmail.com>
2023-05-05 00:02:38 +0800

    add download img

--- Weeb.py
+++ Weeb.py
@@ -47,11 +47,12 @@ time.sleep(2)
 driver.switch_to.default_content()
 Captcha_inner_frame = driver.find_element(By.XPATH,"/html/body/div[8]/div[4]/iframe")
 driver.switch_to.frame(Captcha_inner_frame)
+#Label
 AI_type = driver.find_element(By.XPATH,"//*[@id='"'rc-imageselect'"']/div[2]/div[1]/div[1]/div/strong").text
 img_width = driver.find_element(By.CLASS_NAME,"rc-image-tile-33").get_attribute("naturalWidth")
-print(img_width)
 img_element_url = driver.find_element(By.CLASS_NAME,"rc-image-tile-33").get_attribute("src")
 with open("test.jpeg","wb") as f:
     f.write(requests.get(img_element_url).content)
+
 time.sleep(6)
 driver.close() 
